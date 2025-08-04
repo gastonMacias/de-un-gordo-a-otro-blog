c5c81074606a470995da721defb5b2c454d7d4ee
gastonMacias <gastonezequielmacias@gmail.com>
2025-08-04 19:34:59 -0300

    componente Hero y primeros estilos

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import "./hero.css";
+
+const Hero = () => {
+    return(
+        <>
+        <header className="hero__header">
+            <h1 className="hero__h1">De un <span className="hero__enfasis">gordo</span> a <span className="hero__enfasis">otro</span></h1>
+            <span className="hero__sub-titulo">Un BLOG en el que busco que te sientas comodo y seas parte activa de la comunidad</span>
+        </header>
+        </>
+    )
+}
+
+
+export default Hero;
