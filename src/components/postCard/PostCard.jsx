678a745d2108834939b5d4f44acba49d6c69d6bb
gastonMacias <gastonezequielmacias@gmail.com>
2025-08-21 12:07:34 -0300

    componente PostCard

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,195 @@
+import "./postCard.css";
+
+
+const PostCard = () => {
+    return (
+        <>
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+
+            <div className="postCard">
+                <div className="postCard__image">
+                    <img src="" alt="" />
+                </div>
+                <div className="postCard__body">
+                    <h3 className="postCard__body__title">Titulo</h3>
+                    <span className="postCard__body__who">Quien escribe</span>
+                    <span className="postCard__body__date">21/08/2025</span>
+                    <p className="postCard__body__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quae fuga dolorum pariatur
+                        modi veniam incidunt voluptatem nesciunt rerum unde veritatis officiis, harum iste facere
+                        placeat voluptate amet. Eaque, nemo.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default PostCard;
